4fd45d0d6909ca4d49888423d02c3c1f71888bcb
Neuro-tune <den4ik.kozatskiy@gmail.com>
2025-06-24 00:39:05 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,18 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Google Font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <title>Neuro•Tune — Музыка. Эффекты. Эмоции.</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }

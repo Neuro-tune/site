5ba0de56b8f2bc65ccd11d6477a1c282baca895e
Neuro-tune <den4ik.kozatskiy@gmail.com>
2025-06-24 00:29:21 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}'
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8B3DFF',
+        background: '#0F0F17',
+        card: '#1B1B27'
+      }
+    }
+  },
   plugins: []
-}
+};
